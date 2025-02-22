b7f45a383aa8af5c48f60edfec0adf9ccb95a545
sLiMFly <slimflycode@gmail.com>
2025-02-22 20:12:37 +0100

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# sLiM Gifs
+This is a simple .
+
+# install
+```
+git clone https://github.com/sLiMFly/slim-gifs.git
+yarn
+```
+# usage
+```
+cd sLiM-gifs
+yarn dev
+```
+
+# demo
+https://slimgif.netlify.app/
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -22,7 +22,7 @@ export const AddCategory = ({ onNewCategory }) => {
         <form onSubmit={ onSubmit }>
             <input 
                 type="text"
-                placeholder="Buscar gifs"
+                placeholder="search gifs"
                 value={ inputValue }
                 onChange={ onInputChange }
             />
